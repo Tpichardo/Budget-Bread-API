e40e539b26e87a4933afa4e64db8086c29599dcf
Talia Pichardo <taliapichardo@gmail.com>
2021-06-22 14:58:43 -0400

    revised data

--- models/transactions.js
+++ models/transactions.js
@@ -1,17 +1,62 @@
 module.exports = [
     {
-        date: "April 1",
-        name: "Talia",
-        amount: 0
+        date: "2021-01-26",
+        name: "Birthday",
+        amount: 2000,
+        from: "Zelle payment from EDISON PICHARDO"
     },
     {
-        date: "April 1",
-        name: "Clara",
-        amount: 5
+        date: "2021-01-30",
+        name: "Groceries",
+        amount: -110,
+        from: "TRADER JOES"
     },
     {
-        date: "April 1",
-        name: "Eddy",
-        amount: 4
+        date: "2021-02-01",
+        name: "Rent",
+        amount: -900,
+        from: "COZY"
+    },
+    {
+        date: "2021-02-02",
+        name: "Green Juice",
+        amount: -3,
+        from: "COLUMBIA GOURMET"
+    },
+    {
+        date: "2021-02-05",
+        name: "Salary",
+        amount: 3700,
+        from: "TWITTER"
+    },
+    {
+        date: "2021-02-05",
+        name: "Brunch",
+        amount: -56,
+        from: "HILLTOP PARK ALE HOUSE"
+    },
+    {
+        date: "2021-02-11",
+        name: "Green Juice",
+        amount: -3,
+        from: "COLUMBIA GOURMET"
+    },
+    {
+        date: "2021-02-11",
+        name: "Savings, to the mooooooon!",
+        amount: 1500,
+        from: "ATM CASH DEPOSIT"
+    },
+    {
+        date: "2021-02-11",
+        name: "Mani/Pedi",
+        amount: -50,
+        from: "SHANA NAILS"
+    },
+    {
+        date: "2021-02-12",
+        name: "Dinner with Mami",
+        amount: -80,
+        from: "BENIHANA NY WEST"
     }
 ]

c5a9b87dfb4cff859a91f8a2631a24ab33170a78
Talia Pichardo <taliapichardo@gmail.com>
2022-04-22 19:38:52 -0400

    Adds query to update transactions

--- queries/transactionsQueries.js
+++ queries/transactionsQueries.js
@@ -18,11 +18,11 @@ const getTransactionById = async (id) => {
     }
 };
 
-const addTransaction = async (transaction) => {
+const addTransaction = async (transaction, currentUserId) => {
     try {
         const newTransaction = await db.one(
             "INSERT INTO transactions (current_user_id, transaction_date, transaction_name, transaction_type, transaction_amount, Transaction_vendor) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
-            [transaction.current_user_id, transaction.transaction_date, transaction.transaction_name, transaction.transaction_type, transaction.transaction_amount, transaction.transaction_vendor]
+            [currentUserId, transaction.transaction_date, transaction.transaction_name, transaction.transaction_type, transaction.transaction_amount, transaction.transaction_vendor]
         );
         return newTransaction;
     } catch (error) {
@@ -30,8 +30,22 @@ const addTransaction = async (transaction) => {
     }
 };
 
+const updateTransaction = async (id, transaction) => {
+    try {
+        const updatedTransaction = await db.one(
+            "UPDATE transactions SET current_user_id=$1, transaction_date=$2, transaction_name=$3, transaction_type=$4, transaction_amount=$5, transaction_vendor=$6 where id=$7 RETURNING *",
+            [transaction.current_user_id, transaction.transaction_date, transaction.transaction_name, transaction.transaction_type, transaction.transaction_amount, transaction.transaction_vendor, id]
+        );
+        return updatedTransaction;
+    } catch (error) {
+        return error;
+    }
+};
+
+
 module.exports = {
     getTransactionsByUserId,
     getTransactionById,
-    addTransaction
+    addTransaction,
+    updateTransaction
 };

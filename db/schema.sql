01cbc1ea0abe70616b7b5aad2ebaa7c917418dd3
Talia Pichardo <taliapichardo@gmail.com>
2022-04-21 23:49:17 -0400

    Creates database and transactions table

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bread;
+CREATE DATABASE bread;
+
+\c bread;
+
+CREATE TABLE transactions (
+    user_id TEXT NOT NULL,
+    transaction_date DATE NOT NULL,
+    transaction_name TEXT NOT NULL,
+    transaction_type TEXT NOT NULL, 
+    transaction_amount DECIMAL NOT NULL,
+    vendor TEXT NOT NULL
+);

5c53025858e0aa94f4a86f9c39192c46a62b7862
Talia Pichardo <taliapichardo@gmail.com>
2022-04-21 23:50:28 -0400

    Seeds database with demo transactions

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+\c bread 
+
+INSERT INTO transactions (user_id, transaction_date, transaction_name, transaction_type, transaction_amount, vendor)VALUES
+('uO27zaQLsxYDB3pSRRlsPHAcIkA3', '2021-01-26', 'Birthday', 'Deposit', 2000.00, 'Zelle payment from EDISON PICHARDO'),
+('uO27zaQLsxYDB3pSRRlsPHAcIkA3', '2021-01-30', 'Groceries', 'Expense', 110.49, 'TRADER JOES'),
+('uO27zaQLsxYDB3pSRRlsPHAcIkA3', '2021-02-01', 'Rent', 'Expense', 954.00, 'COZY'),
+('uO27zaQLsxYDB3pSRRlsPHAcIkA3', '2021-02-02', 'Green Juice', 'Expense', 3.12, 'COLUMBIA GOURMET'),
+('uO27zaQLsxYDB3pSRRlsPHAcIkA3', '2021-02-05', 'Brunch', 'Expense', 43.18, 'HILLTOP PARK ALE HOUSE'),
+('uO27zaQLsxYDB3pSRRlsPHAcIkA3', '2021-02-11', 'Green Juice', 'Expense', 3.12, 'COLUMBIA GOURMET'),
+('uO27zaQLsxYDB3pSRRlsPHAcIkA3', '2021-02-11', 'Savings, to the mooooooon!', 'Deposit', 1500.00, 'ATM CASH DEPOSIT'),
+('uO27zaQLsxYDB3pSRRlsPHAcIkA3', '2021-02-12', 'Dinner with Mami', 'Expense', 80.67, 'BENIHANA NY WEST');

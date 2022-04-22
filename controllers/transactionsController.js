7425cb7c5710e09ad04dafa7e79ff8cc61fb69ef
Talia Pichardo <taliapichardo@gmail.com>
2022-04-22 15:35:28 -0400

    Adds functionality to get transaction by id

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const getTransactionsByUserId = require('../queries/transactionsQueries');
+const {
+    getTransactionsByUserId,
+    getTransactionById
+} = require('../queries/transactionsQueries');
 
 const transactions = express.Router();
 
@@ -14,40 +17,19 @@ transactions.get('/', async (req, res) => {
     }
 });
 
-transactions.get('/:index', (req, res) => {
-    const { index } = req.params
-    if (transactionsArr[index]) {
-        res.status(200).json(transactionsArr[index])
-    } else {
-        res.redirect('/404')
+transactions.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const transaction = await getTransactionById(id);
+        if (transaction.id) {
+            return res.json(transaction);
+        } else {
+            console.log(`Database error: ${transaction}`);
+            throw `There is no transaction with id: ${id}`
+        }
+    } catch (error) {
+        res.status(404).json({ error: "Transaction not found.", message: error });
     }
-})
-
-transactions.post('/', (req, res) => {
-    transactionsArr.push(req.body)
-    //send back the new resource as confirmation
-    res.json(transactionsArr[transactionsArr.length - 1])
-})
-
-transactions.put('/:index', (req, res) => {
-    const { index } = req.params
-    if (transactionsArr[index]) {
-        transactionsArr[index] = req.body
-        res.json(transactionsArr[index])
-    } else {
-        res.redirect('/404')
-    }
-})
-
-transactions.delete('/:index', (req, res) => {
-    const { index } = req.params
-    if (transactionsArr[index]) {
-        const deletedTransaction = transactionsArr.splice(index, 1)
-        res.json(deletedTransaction[0])
-    } else {
-        res.redirect('/404')
-    }
-})
-
+});
 
 module.exports = transactions;

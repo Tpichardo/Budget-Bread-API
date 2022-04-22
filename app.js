5fa1c663e27f924beb920172639cd08af63a8ee2
Talia Pichardo <taliapichardo@gmail.com>
2022-04-22 00:18:43 -0400

    Adds semicolons

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const transactionsController = require('./controllers/transactionsController')
+const transactionsController = require('./controllers/transactionsController');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/transactions', transactionsController)
+app.use('/transactions', transactionsController);
 
 app.use((req, res, next) => {
     console.log("INCOMING REQUEST:", req.method, req.path)
     next()
-})
+});
 
-// Routes
 app.get("/", (req, res) => {
     res.send("Welcome to the budgeting app!");
 });
 
 app.get("*", (req, res) => {
-    res.status(404).send("Sorry mate, page not found")
-})
+    res.status(404).send("Sorry mate, page not found");
+});
 
 
 
-module.exports = app
+module.exports = app;

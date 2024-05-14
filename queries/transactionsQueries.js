f2b2230413a9f6b9de6a90bee6a9495fa632c099
Talia Pichardo <taliapichardo@gmail.com>
2024-05-14 02:23:29 -0400

    remove trycatch from queries folder to improve error handling, quesries throw, controllers catch

--- queries/transactionsQueries.js
+++ queries/transactionsQueries.js
@@ -1,61 +1,64 @@
-const db = require('../db/dbConfig.js');
+const db = require("../db/dbConfig.js");
 
 const getTransactionsByUserId = async (current_user_id) => {
-    try {
-        const userTransactions = await db.any("SELECT * FROM transactions WHERE current_user_id = $1", current_user_id);
-        return userTransactions;
-    } catch (error) {
-        return error;
-    }
+	const userTransactions = await db.any(
+		"SELECT * FROM transactions WHERE current_user_id = $1",
+		current_user_id
+	);
+	return userTransactions;
 };
 
 const getTransactionById = async (id) => {
-    try {
-        const transaction = await db.one("SELECT * FROM transactions WHERE id = $1", id);
-        return transaction;
-    } catch (error) {
-        return error;
-    }
+	const transaction = await db.one(
+		"SELECT * FROM transactions WHERE id = $1",
+		id
+	);
+	return transaction;
 };
 
 const addTransaction = async (transaction) => {
-    try {
-        const newTransaction = await db.one(
-            "INSERT INTO transactions (current_user_id, transaction_date, transaction_name, transaction_type, transaction_amount, Transaction_vendor) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
-            [transaction.current_user_id, transaction.transaction_date, transaction.transaction_name, transaction.transaction_type, transaction.transaction_amount, transaction.transaction_vendor]
-        );
-        return newTransaction;
-    } catch (error) {
-        return error;
-    }
+	const newTransaction = await db.one(
+		"INSERT INTO transactions (current_user_id, transaction_date, transaction_name, transaction_type, transaction_amount, Transaction_vendor) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+		[
+			transaction.current_user_id,
+			transaction.transaction_date,
+			transaction.transaction_name,
+			transaction.transaction_type,
+			transaction.transaction_amount,
+			transaction.transaction_vendor,
+		]
+	);
+	return newTransaction;
 };
 
 const updateTransaction = async (id, transaction) => {
-    try {
-        const updatedTransaction = await db.one(
-            "UPDATE transactions SET current_user_id=$1, transaction_date=$2, transaction_name=$3, transaction_type=$4, transaction_amount=$5, transaction_vendor=$6 where id=$7 RETURNING *",
-            [transaction.current_user_id, transaction.transaction_date, transaction.transaction_name, transaction.transaction_type, transaction.transaction_amount, transaction.transaction_vendor, id]
-        );
-        return updatedTransaction;
-    } catch (error) {
-        return error;
-    }
+	const updatedTransaction = await db.one(
+		"UPDATE transactions SET current_user_id=$1, transaction_date=$2, transaction_name=$3, transaction_type=$4, transaction_amount=$5, transaction_vendor=$6 where id=$7 RETURNING *",
+		[
+			transaction.current_user_id,
+			transaction.transaction_date,
+			transaction.transaction_name,
+			transaction.transaction_type,
+			transaction.transaction_amount,
+			transaction.transaction_vendor,
+			id,
+		]
+	);
+	return updatedTransaction;
 };
 
 const deleteTransaction = async (id) => {
-    try {
-        const deletedSong = await db.one("DELETE FROM transactions WHERE id=$1 RETURNING * ", id);
-        return deletedSong;
-    } catch (error) {
-        return error;
-    }
+	const deletedSong = await db.one(
+		"DELETE FROM transactions WHERE id=$1 RETURNING * ",
+		id
+	);
+	return deletedSong;
 };
 
-
 module.exports = {
-    getTransactionsByUserId,
-    getTransactionById,
-    addTransaction,
-    updateTransaction,
-    deleteTransaction
+	getTransactionsByUserId,
+	getTransactionById,
+	addTransaction,
+	updateTransaction,
+	deleteTransaction,
 };

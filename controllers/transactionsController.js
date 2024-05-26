ac1a04b17e0d2993b4ee0dc6d61b420e96f79d51
Talia Pichardo <taliapichardo@gmail.com>
2024-05-25 22:07:58 -0400

    Add user input validation to post handler function, and update status code to be returned for a post request

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -9,6 +9,32 @@ const {
 
 const transactions = express.Router();
 
+const TRANSACTION_FIELDS = {
+	current_user_id: true,
+	transaction_amount: true,
+	transaction_date: true,
+	transaction_name: true,
+	transaction_type: true,
+	transaction_vendor: true,
+};
+
+const isvalidTransaction = (transaction) => {
+	for (let field in TRANSACTION_FIELDS) {
+		if (!transaction.hasOwnProperty(field)) {
+			return false;
+		}
+	}
+
+	for (let field in transaction) {
+		if (!TRANSACTION_FIELDS[field]) {
+			console.log(field, "here");
+			return false;
+		}
+	}
+
+	return true;
+};
+
 transactions.get("/", async (req, res) => {
 	try {
 		const { current_user_id } = req.query;
@@ -36,15 +62,18 @@ transactions.get("/:id", async (req, res) => {
 
 transactions.post("/", async (req, res) => {
 	try {
-		const newTransaction = await addTransaction(req.body);
-		if (newTransaction.id) {
-			res.json(newTransaction);
-		} else {
-			console.log(req.body);
-			throw `Error adding ${req.body} to the database.`;
+		const transaction = req.body;
+		if (!isvalidTransaction(transaction)) {
+			return res.status(400).json({
+				error: `transaction must only have fields: ${Object.keys(
+					TRANSACTION_FIELDS
+				).join(", ")}`,
+			});
 		}
+		const newTransaction = await addTransaction(transaction);
+		res.status(201).json({ data: newTransaction });
 	} catch (error) {
-		res.status(404).json({ error: error });
+		res.status(500).json({ error: error.message });
 	}
 });
 
